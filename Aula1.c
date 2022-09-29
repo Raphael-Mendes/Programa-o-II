4503b4b133faec1d325ddea3bdcf08b3f2e5821e
Raphael-Mendes <raphael_m@id.uff.br>
2022-09-29 17:18:37 -0300

    aula 3

--- Aula1.c
+++ Aula1.c
@@ -1,20 +1,28 @@
-/* aula 1
+
 #include <stdio.h>
 
+// Base:
+/* 
+int main(){
+    return 0;
+}
+*/
+
+/* Aula 1 - Introdução a Ponteiros
 int main(){
     int x, y;
     int *p;
+    //ponteiro a partir de variáveis:
     p = &x;
     x=10;
     y = 20;
     *p = y;
     printf("%d %d", x, y);
-
     return 0;
 }
 */
 
-//aula 2
+/*aula 2 Ponteiros com double e endereços de memória
 #include <stdio.h>
 
 int main(){
@@ -26,7 +34,7 @@ int main(){
     
     int *px;
     double *py;
-    
+    // Ponteiros a partir de endereços de memória
     px = 6422284;
     py = 6422272;
 
@@ -35,5 +43,23 @@ int main(){
     printf("A soma do X e do Y é %f \n", soma);
     printf("o valor de x é %d", *px);
     printf("o valor de x é %f", *py);
+    return 0;
+}
+*/
+
+// Aula 3 - Vetores
+int main(void){
+    float nota[5], total, media;
+    for(int i = 0; i<5; ++i) {
+        scanf("%f", &nota[i]);
+        printf("\n %f\n", nota[i]);
+    }
+    for(int i = 0; i<5; ++i){
+        total += nota[i];
+    }
+    media = total/5;
+    printf("sua média é %f\n", media);
+    printf("%f", nota);
+
     return 0;
 }
